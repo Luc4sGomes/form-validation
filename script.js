868bf872b9260fd5bf9ebbc24f4626135431c766
lucas <lucasgominhaa448@gmail.com>
2021-12-11 16:52:31 -0300

    added changes and new functions

--- script.js
+++ script.js
@@ -1,40 +1,77 @@
-const fields = document.querySelectorAll('[required]') //procurando todas as tagas que tem o atributo required
-console.log(fields)
+const fields = document.querySelectorAll("[required]"); //procurando todas as tagas que tem o atributo required
+console.log(fields);
+
+function validateField(field) {
+  function verifyErrors() {
+    let foundError = false;
+
+    for (let error in field.validity) {
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error;
+      }
+    }
+
+    console.log(foundError);
+
+    return foundError;
+  }
+
+  function setCustomMessage(message) {
+    if (message) {
+      spanError.classList.add("active");
+      spanError.innerHTML = "campo obrigatorio";
+    } else {
+      spanError.classList.remove("active");
+      spanError.innerHTML = "";
+    }
+  }
+
+  return function () {
+    if (verifyErrors()) {
+      setCustomMessage("Campo Obrigatorio");
+    } else {
+      setCustomMessage();
+    }
+  };
+}
 
 function customValidation(event) {
-  const field = event.target
+  //eliminar o bubble
+  event.preventDefault();
+
+  const field = event.target;
+  const validation = valideField(field);
 
   //logica para verificar se existem erros
 
   function verifyErrors() {
-    let foundError = false
+    let foundError = false;
 
     for (let error in field.validity) {
-      if (error != 'customError' && field.validity[error]) {
-        foundError = error
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error;
       }
     }
 
-    return foundError
+    console.log(foundError);
+
+    return foundError;
   }
 
-  const error = verifyErrors()
-  console.log('erro existe: ', error)
+  console.log("erro existe: ", error);
 
-  if (error) {
-    //trocar mensagem de required
-    field.setCustomValidity('este campo é obrigatorio')
-  }
-  else {
-    field.setCustomValidity("");
-  }
+  const spanError = field.parentNode.querySelector("span.error");
 }
 
 for (let field of fields) {
-  field.addEventListener('invalid', customValidation)
+  field.addEventListener("invalid", event => {
+    event.preventDefault();
+    customValidation(event);
+  });
+  field.addEventListener("blur", customValidation);
 }
 
-document.querySelector('form').addEventListener('submit', event => {
-  console.log('enviado o form')
-  event.preventDefault() //não envia o formulario
-})
+document.querySelector("form").addEventListener("submit", event => {
+  console.log("enviado o form");
+  event.preventDefault(); //não envia o formulario
+});
